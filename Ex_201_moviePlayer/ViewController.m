93021481e1e7d92e63d8f47575e5c9cdbb84ea67
saerem21 <nemessisdds>
2014-01-16 14:35:56 +0900

    동영상 재생입니다.
    근데 작동을 안하는데…

--- Ex_201_moviePlayer/ViewController.m
+++ Ex_201_moviePlayer/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+
+#define MOVIE_URL @"http://movies.apple.com/media/kr/iphone/2011/tours/apple-iphone4s-feature_keynote-kr20111110_r848-9cie,mov"
 
 @interface ViewController ()
+{
+    MPMoviePlayerController *_player;
+}
+
 
 @end
 
@@ -18,6 +25,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleMediaTypeNoti:) name:@"MPMovieSourceTypeAvailableNotification" object:nil];
+    //NSURL *url = [[NSBundle mainBundle]URLForResource:@"movie" withExtension:@"mp4"];
+    NSURL *url = [NSURL URLWithString:MOVIE_URL];
+    NSAssert((nil!=url), @"url is null");
+    
+    _player = [[MPMoviePlayerController alloc] initWithContentURL:url];
+    [_player prepareToPlay];
+    [_player.view setFrame:self.view.bounds];
+    [self.view addSubview:_player.view];
+    
+}
+
+-(void)handleMediaTypeNoti:(NSNotification *)noti{
+    MPMoviePlayerController *p = noti.object;
+    NSLog(@"Media Source Type: %d",p.movieSourceType);
+    
+}
+
+-(void)handleStateChanged:(NSNotification *)noti{
+    MPMoviePlayerController *p = noti.object;
+    NSLog(@"playBack state %d",p.playbackState);
+}
+
+-(void)viewDidUnload{
+    [super viewDidUnload];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)didReceiveMemoryWarning
